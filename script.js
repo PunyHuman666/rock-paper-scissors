63f862baf78880591a32d39e5b365b69ea306d1d
PunyHuman666 <geutom335@gmail.com>
2024-02-06 12:15:54 +0100

    Finish 5 round game in console

--- script.js
+++ script.js
@@ -1,51 +1,80 @@
 let choiceCpu;
-let choicePlayer = prompt("Rock, Paper or Scissors?");
 let result;
+let score;
+let playerScore = 0;
+let cpuScore = 0;
+let round = 1;
 
-function caseSensitive() {
-    return choicePlayer.toLocaleUpperCase().charAt(0).toUpperCase() + choicePlayer.slice(1);
+function playGame() {
+
+    while (round < 6) {
+
+        console.log(`Round: ${round}`);
+
+        let choicePlayer = prompt("Rock, Paper or Scissors?");
+
+        function caseSensitive() {
+            choicePlayer = choicePlayer.toLowerCase();
+            return choicePlayer.charAt(0).toUpperCase() + choicePlayer.slice(1);
+        }
+
+        if (caseSensitive() === "Rock" || caseSensitive() === "Paper" || caseSensitive() === "Scissors") {
+
+            function getComputerChoice() {
+                let random = Math.floor((Math.random() * (3 - 1 + 1)) + 1);
+                if (random === 1) {
+                    choiceCpu = "Rock";
+                    console.log(`CPU: ${choiceCpu}`);
+                    return choiceCpu;
+                } else if (random === 2) {
+                    choiceCpu = "Paper";
+                    console.log(`CPU: ${choiceCpu}`);
+                    return choiceCpu;
+                } else {
+                    choiceCpu = "Scissors";
+                    console.log(`CPU: ${choiceCpu}`);
+                    return choiceCpu;
+                }
+            }
+
+            function playRound(playerSelection, computerSelection) {
+                if ((choiceCpu === "Rock" && choicePlayer === "scissors")
+                    || (choiceCpu === "Paper" && choicePlayer === "rock")
+                    || (choiceCpu === "Scissors" && choicePlayer === "paper")
+                    ) {
+                        cpuScore++;
+                        result = `You lose! ${choiceCpu} beats ${caseSensitive()}!\nScore:\nPlayer = ${playerScore} : CPU = ${cpuScore}\n `;
+                        return result;
+                } else if ((choiceCpu === "Rock" && choicePlayer === "paper")
+                    || (choiceCpu === "Paper" && choicePlayer === "scissors")
+                    || (choiceCpu === "Scissors" && choicePlayer === "rock")
+                    ) {
+                        playerScore++;
+                        result = `You won! ${caseSensitive()} beats ${choiceCpu}!\nScore:\nPlayer = ${playerScore} : CPU = ${cpuScore}\n `;
+                        return result;
+                } else {
+                    result = `It's a tie!\nScore:\nPlayer = ${playerScore} : CPU = ${cpuScore}\n `;
+                    return result;
+                }
+            }
+
+            console.log(`Player: ${caseSensitive()}`);
+            console.log(playRound(choicePlayer, getComputerChoice()));
+
+            round++;
+
+        } else {
+            alert("Wrong entry!");
+        }
+    }
 }
 
-if (caseSensitive() === "Rock" || caseSensitive() === "Paper" || caseSensitive() === "Scissors") {
+playGame();
 
-
-    function getComputerChoice() {
-        let random = Math.floor((Math.random() * (3 - 1 + 1)) + 1);
-        if (random === 1) {
-            choiceCpu = "Rock";
-            console.log(`CPU: ${choiceCpu}`);
-            return choiceCpu;
-        } else if (random === 2) {
-            choiceCpu = "Paper";
-            console.log(`CPU: ${choiceCpu}`);
-            return choiceCpu;
-        } else {
-            choiceCpu = "Scissors";
-            console.log(`CPU: ${choiceCpu}`);
-            return choiceCpu;
-        }
-    }
-
-    function playRound(playerSelection, computerSelection) {
-        if ((choiceCpu === "Rock" && choicePlayer === "scissors")
-            || (choiceCpu === "Paper" && choicePlayer === "rock")
-            || (choiceCpu === "Scissors" && choicePlayer === "paper")
-            ) {
-                result = `You lose! ${choiceCpu} beats ${caseSensitive()}!`
-                return result;
-        } else if ((choiceCpu === "Rock" && choicePlayer === "paper")
-            || (choiceCpu === "Paper" && choicePlayer === "scissors")
-            || (choiceCpu === "Scissors" && choicePlayer === "rock")
-            ) {
-                result = `You won! ${caseSensitive()} beats ${choiceCpu}!`
-                return result;
-        } else {
-            result = `It's a tie!`
-            return result;
-        }
-    }
-
-    console.log(playRound(choicePlayer, getComputerChoice()));
+if (playerScore > cpuScore) {
+    console.log("CONGRATULATIONS! YOU WON!");
+} else if (playerScore < cpuScore) {
+    console.log("You lose! Sorry :(");
 } else {
-    alert("Wrong entry!");
+    console.log("It's a tie! Try again?");
 }
